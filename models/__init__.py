76137b957b09f9b9e4359c656abb8025bf905d20
Matia-k <matia40k@gmail.com>
2023-07-16 08:23:34 -0700

    updated documentation of the init module

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-from models.engine.file_storage import FileStorage
 """Initializes the models module"""
+from models.engine.file_storage import FileStorage
 
 
 storage = FileStorage()
