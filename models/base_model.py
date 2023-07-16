be59c7ff6b27f48de50e1457eb40ef525972dfd0
Matia-k <matia40k@gmail.com>
2023-07-16 06:17:36 -0700

    created a base class BaseModel

---
+++ models/base_model.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+from uuid import uuid4
+from datetime import datetime
+import models
+"""."""
+
+
+class BaseModel:
+    """defines all common attributes/methods for other classes.
+
+    """
+    def __init__(self, *args, **kwargs):
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+        time_form = "%Y-%m-%dT%H:%M:%S.%f"
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    self.__dict__[k] = datetime.strptime(v, time_form)
+                elif k == "__class__":
+                    continue
+                else:
+                    self.__dict__[k] = v
+        else:
+            models.storage.new(self)
+
+    def __str__(self):
+        """Print the string representation of a base model instance"""
+        cl_name = self.__class__.__name__
+        return "[{}] ({}) {}".format(cl_name, self.id, self.__dict__)
+
+    def save(self):
+        """updates updated_at with the current datetime"""
+        self.updated_at = datetime.now()
+        models.storage.save()
+
+    def to_dict(self):
+        """returns a dict containing all k:v of __dict__ of the instance"""
+        n_dict = self.__dict__.copy()
+        n_dict["__class__"] = self.__class__.__name__
+        n_dict["created_at"] = self.created_at.isoformat()
+        n_dict["updated_at"] = self.updated_at.isoformat()
+        return n_dict
