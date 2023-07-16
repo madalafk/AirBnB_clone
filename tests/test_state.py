56bd51e08c77832de9f13d5c62a864ede9cf74aa
fmadala@hotmail.com <fmadala@hotmail.com>
2023-07-16 10:06:12 -0700

    test_state.py

---
+++ tests/test_state.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Defines unittests for models/state.py.
+
+Unittest classes:
+    class TestState
+"""
+
+import unittest
+from models.state import State
+
+class TestState(unittest.TestCase):
+    def test_name_initialization(self):
+        state = State()
+        self.assertEqual(state.name, "")
+
+    def test_name_assignment(self):
+        state = State()
+        state.name = "New Mexico"
+        self.assertEqual(state.name, "New Mexico")
+
+if __name__ == '__main__':
+    unittest.main()
